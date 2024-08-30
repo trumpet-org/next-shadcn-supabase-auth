ce85da2753e266c4d3a28d6b29ac377813bd856b
isaaclindenman <30323315+isaaclindenman@users.noreply.github.com>
2024-08-30 17:33:21 +0300

    Revert "feat: fixed shadcn and create basic hero section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-
+import { PagePath } from "@/config/enums";
 import { getServerClient } from "@/utils/supabase/server";
 import Link from "next/link";
-import { Button, buttonVariants } from "gen/ui/button";
-import { cn } from "gen/cn";
-import { redirect } from "next/navigation";
 
 export default async function LandingPage() {
 	const supabase = getServerClient();
@@ -13,28 +9,15 @@ export default async function LandingPage() {
 		data: { user },
 	} = await supabase.auth.getUser();
 
-	if (user) {
-		redirect(`/${user.id}`);
-	}
-
 	return (
-		<section className="container flex flex-col md:flex-row md:h-[calc(100dvh-10rem)] h-[calc(100dvh-8rem)]">
-			<div className="w-full md:w-1/2  pt-8 flex flex-col gap-3 md:gap-4 justify-center text-center md:text-left">
-				<h1 className="text-balance text-5xl font-extrabold tracking-tight sm:text-6xl md:text-7xl ">
-					Get to month three on day one
+		<section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+			<div className="flex max-w-[980px] flex-col items-start gap-2">
+				<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+					Fully featured Next.js template.
 				</h1>
-				<p className="text-sm max-w-screen-md text-balance  text-muted-foreground sm:text-xl sm:leading-8">
-					BaseMind is a world class SaaS project Starter, clean code with 95% test coverage.
-				</p>
-				<Link href="/auth">
-					<Button className={cn(buttonVariants({ variant: "default", size: "lg" }))}>Get Started</Button>
-				</Link>
+				<p className="max-w-[700px] text-lg text-muted-foreground">Next.js 14 Ready.</p>
 			</div>
-			<div className="w-full md:w-1/2 flex items-center justify-center">
-				<div className="aspect-video w-full max-w-2xl">
-					<Image width="1000" height="600" src="/images/screenshot.png" alt={"screenshot"} className="w-full h-auto" />
-				</div>
-			</div>
+			<div className="flex gap-4">{user ? "Logged in" : <Link href={PagePath.AUTH}>Log In</Link>}</div>
 		</section>
 	);
 }
