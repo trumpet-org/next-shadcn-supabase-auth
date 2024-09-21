bb13dbdf91e810dac0215b82d6c1e3286ce91c41
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-09-21 12:12:06 +0200

    chore: adjustments for rebase

--- dictionaries/dictionaries.ts
+++ dictionaries/dictionaries.ts
@@ -1,11 +1,10 @@
 "server-only";
 import type { Locale } from "./i18n-config";
 
-
 const dictionaries = {
-    en: () => import("./en.json").then((module) => module.default),
+	en: () => import("./en.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) =>
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    dictionaries[locale]?.() ?? dictionaries.en();
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	dictionaries[locale]?.() ?? dictionaries.en();
--- src/middleware/i18n-middleware.ts
+++ src/middleware/i18n-middleware.ts
@@ -1,26 +1,25 @@
-import { i18n } from '../../dictionaries/i18n-config';
-import { NextResponse, type NextRequest } from "next/server";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
+import { type NextRequest, NextResponse } from "next/server";
+import { i18n } from "../../dictionaries/i18n-config";
 
 function getLocale(request: NextRequest): string | undefined {
-  const { locales, defaultLocale } = i18n;
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (locales.length < 2) {
-    return defaultLocale;
-  }
-  const negotiatorHeaders: Record<string, string> = {};
-  for (const [key, value] of request.headers.entries()) {
-    // biome-ignore lint/suspicious/noAssignInExpressions: Necessary assignment within expression
-    negotiatorHeaders[key] = value;
-  }
+	const { locales, defaultLocale } = i18n;
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	if (locales.length < 2) {
+		return defaultLocale;
+	}
+	const negotiatorHeaders: Record<string, string> = {};
+	for (const [key, value] of request.headers.entries()) {
+		negotiatorHeaders[key] = value;
+	}
 
-  // @ts-expect-error locales are readonly
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(locales);
+	// @ts-expect-error locales are readonly
+	const languages = new Negotiator({ headers: negotiatorHeaders }).languages(locales);
 
-  const locale = matchLocale(languages, locales, i18n.defaultLocale);
+	const locale = matchLocale(languages, locales, i18n.defaultLocale);
 
-  return locale;
+	return locale;
 }
 
 /**
@@ -29,21 +28,18 @@ function getLocale(request: NextRequest): string | undefined {
  * @returns The response object.
  */
 export function i18nMiddleware(request: NextRequest) {
-  const { pathname, search } = request.nextUrl;
+	const { pathname, search } = request.nextUrl;
 
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
+	const pathnameIsMissingLocale = i18n.locales.every(
+		(locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+	);
 
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}${search}`,
-        request.url
-      )
-    );
-  }
+	if (pathnameIsMissingLocale) {
+		const locale = getLocale(request);
+		return NextResponse.redirect(
+			new URL(`/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}${search}`, request.url),
+		);
+	}
 
-  return NextResponse.next();
+	return NextResponse.next();
 }
